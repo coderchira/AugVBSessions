752632790d84ef49d8efbce30216f2d644f9ecc1
coderchira <ruchira.aggarwal@gmail.com>
2015-08-05 17:35:54 +0530

    CollectionsExercise

--- AugustVBSession/CollectionsExercise.vb
+++ AugustVBSession/CollectionsExercise.vb
@@ -5,5 +5,31 @@
     'code such that : transfer of all the names from one collection to another 
     'except the one which has your name
 
+    'Module level variable
+    Dim myColl As New Collection
+    Dim newcoll As New Collection
+    Sub AddCollection()
+        myColl.Add("Priya")
+        myColl.Add("Rekha")
+        myColl.Add("Amit")
+        myColl.Add("Sasmita")
+        myColl.Add("Kishore")
+        myColl.Add("Junaid")
+        myColl.Add("Vani")
+        myColl.Add("Srikant")
+        myColl.Add("Ruchira")
+        myColl.Add("Agnes")
+
+        For Each item In myColl
+            If Not item.Equals("Rekha") Then
+                newcoll.Add(item)
+            End If
+        Next
+        For Each item In newcoll
+            Console.WriteLine(item)
+        Next
+        Console.ReadLine()
+    End Sub
+
 
 End Module
