a4da01f45be93a9e7496fbf2fb48f92c2fe64948
coderchira <ruchira.aggarwal@gmail.com>
2015-08-05 16:52:30 +0530

    Forms

---
+++ MyVBForm/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnClick
+        '
+        Me.btnClick.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnClick.Location = New System.Drawing.Point(64, 117)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(75, 23)
+        Me.btnClick.TabIndex = 0
+        Me.btnClick.Text = "ClickMe"
+        Me.btnClick.UseVisualStyleBackColor = False
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(53, 71)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(100, 20)
+        Me.txtBox.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.btnClick)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnClick As System.Windows.Forms.Button
+    Friend WithEvents txtBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ MyVBForm/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        MessageBox.Show("The form is clicked")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnClick.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        MessageBox.Show("The button is clicked")
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtBox.TextChanged
+        btnClick.Enabled = True
+        txtBox.ForeColor = Color.Red
+    End Sub
+
+
+End Class
