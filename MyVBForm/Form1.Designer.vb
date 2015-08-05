cfb366d323f0075b006f30774dddea82f5d139bb
coderchira <ruchira.aggarwal@gmail.com>
2015-08-05 16:55:13 +0530

    Forms

--- MyVBForm/Form1.Designer.vb
+++ MyVBForm/Form1.Designer.vb
@@ -24,30 +24,50 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.btnClick = New System.Windows.Forms.Button()
         Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'btnClick
         '
         Me.btnClick.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.btnClick.Location = New System.Drawing.Point(64, 117)
+        Me.btnClick.Location = New System.Drawing.Point(118, 18)
         Me.btnClick.Name = "btnClick"
         Me.btnClick.Size = New System.Drawing.Size(75, 23)
         Me.btnClick.TabIndex = 0
-        Me.btnClick.Text = "ClickMe"
+        Me.btnClick.Text = "Search"
         Me.btnClick.UseVisualStyleBackColor = False
         '
         'txtBox
         '
-        Me.txtBox.Location = New System.Drawing.Point(53, 71)
+        Me.txtBox.Location = New System.Drawing.Point(12, 21)
         Me.txtBox.Name = "txtBox"
         Me.txtBox.Size = New System.Drawing.Size(100, 20)
         Me.txtBox.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 57)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(100, 147)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(145, 57)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(106, 147)
+        Me.ListBox2.TabIndex = 3
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.txtBox)
         Me.Controls.Add(Me.btnClick)
         Me.Name = "Form1"
@@ -58,5 +78,7 @@ Partial Class Form1
     End Sub
     Friend WithEvents btnClick As System.Windows.Forms.Button
     Friend WithEvents txtBox As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
 
 End Class
